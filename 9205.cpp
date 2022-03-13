5e0c0f25c4173fe55ccdffda1a78a66b827492f2
iodoyo16 <durfbgksmsskfro@gmail.com>
2022-03-13 23:56:11 +0900

    bfs

--- 9205.cpp
+++ 9205.cpp
@@ -1,17 +1,52 @@
 #define _CRT_SECURE_NO_WARNINGS
+#define INF 999999999
 #include<stdio.h>
-
+#include<vector>
+#include<queue>
 using namespace std;
-
-
+typedef pair<int, int> ii;
+queue<int>q;
+ii pos[110];
+int visit[110];
+vector<vector <int> >v;
 int main() {
 	int t, n, m;
 	scanf("%d", &t);
 	while (t--) {
+		int x,y;
 		scanf("%d", &n);
-		for (int i = 0; i < n; i++) {
-
+		v.resize(n + 3);
+		for (int i = 0; i <= n+1; i++) {
+			scanf("%d %d", &x, &y);
+			pos[i] = ii(x, y);
+			for (int j = 0; j < i; j++) {
+				int cur_dist=abs(pos[j].first - x)+ abs(pos[j].second - y);
+				if (cur_dist <=1000) {
+					v[i].push_back(j);
+					v[j].push_back(i);
+				}
+			}
+			visit[i] = 0;
 		}
+		q.push(0);
+		visit[0] = 1;
+		while (!q.empty()) {
+			int cur = q.front();
+			q.pop();
+			for (int i = 0; i < v[cur].size(); i++) {
+				int next = v[cur][i];
+				if (visit[next] == 0) {
+					visit[next] = 1;
+					q.push(next);
+				}
+			}
+		}
+		if (visit[n + 1] == 1)
+			printf("happy\n");
+		else
+			printf("sad\n");
+		for (int i = 0; i <= n + 1; i++)
+			v[i].clear();
 	}
 }
 
